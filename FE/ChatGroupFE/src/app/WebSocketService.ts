5a0b61ca7de21762e88ea8ca6463583e5b1e8527
SamantaBeqiri <samanta29beqiri@gmail.com>
2019-07-22 21:45:10 +0200

    Web socket connection

---
+++ FE/ChatGroupFE/src/app/WebSocketService.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+//import { FilterWebSocketService } from './filter-web-socket.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService {
+
+  
+  
+  disabled = true;
+ 
+  private stompClient = null;
+ 
+  constructor() {
+  }
+ 
+  setConnected(connected: boolean) {
+    this.disabled = !connected;
+ 
+    if (connected) {
+    }
+  }
+ 
+  connect() {
+    let socket = new SockJS("http://localhost:8080//socketMessage/");
+    this.stompClient = Stomp.over(socket);
+  
+
+    this.stompClient.connect({}, (frame)=> {
+      this.stompClient.subscribe("/ChatGroups/create", (message) => {
+
+        console.log("message")
+        console.log(message.body)
+      // this.filterWebSocketService.emitFilter(filterInfo);
+    });
+      
+
+        
+     
+    });
+  }
+ 
+  disconnect() {
+    if (this.stompClient != null) {
+      this.stompClient.disconnect();
+    }
+ 
+    this.setConnected(false);
+    console.log('Disconnected!');
+  }
+ 
+  
+ 
+  
+}
--- FE/ChatGroupFE/src/app/app.component.ts
+++ FE/ChatGroupFE/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebSocketService } from './WebSocketService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(  private socketService:WebSocketService){
+
+
+  }
+
+  ngOnInit(): void {
+    console.log("hello")
+    this.socketService.connect()
+  }
   title = 'ChatGroupFE';
+  
+
 }
