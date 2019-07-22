25a9099116d739211ce2f8754657a94e89fc16af
Nikolin Koco <nikolin.koco@outlook.com>
2019-07-22 15:59:43 +0200

    first

---
+++ BE/src/main/java/com/example/demo/services/ChatgroupsService.java
@@ -0,0 +1,41 @@
+package com.example.demo.services;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("ChatsGroupsCollection")
+public class ChatgroupsService {
+    @Id
+    private String id;
+    private String name;
+    private String type;
+
+    public ChatGroupsModel() {
+
+    }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+    }
+}
