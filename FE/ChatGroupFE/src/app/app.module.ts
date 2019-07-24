167333cb744defa93090fdfc678d8b2c554c434f
Nikolin Koco <nikolin.koco@outlook.com>
2019-07-24 17:34:23 +0200

    merge

--- FE/ChatGroupFE/src/app/app.module.ts
+++ FE/ChatGroupFE/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AddGroupComponent } from './add-group/add-group.component';
 import { DeleteComponent } from './delete/delete.component';
-import { ChatComponent } from './chat/chat.component';
+import {ChatComponent} from './chat/chat.component';
 import { MessageFieldComponent } from './message-field/message-field.component';
-import {MaterialModule} from "./material.module";
-import {ReactiveFormsModule} from "@angular/forms";
+import {MaterialModule} from './material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,9 +20,11 @@ import {ReactiveFormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule,
     ReactiveFormsModule,
-],
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- FE/ChatGroupFE/src/app/chat/chat.component.ts
+++ FE/ChatGroupFE/src/app/chat/chat.component.ts
@@ -1,18 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit, Inject} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
-import { WebSocketService } from '../WebSocketService';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {WebSocketService} from '../WebSocketService';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material";
+
+export interface DialogData {
+  username: string;
+}
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewInit {
   public chatGroups = [];
   public chatMessages = [];
   messageForm: FormGroup;
+  public username;
 
-  constructor(private aaa: WebSocketService) {
+  constructor(private aaa: WebSocketService, public dialog: MatDialog) {
   }
 
   ngOnInit() {
@@ -23,10 +30,45 @@ export class ChatComponent implements OnInit {
     this.chatMessages = ['msg1', 'msg2', 'msg3'];
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.openDialog();
+    }, 300);
+  }
 
   sendMessage() {
     const chatMessage = this.messageForm.getRawValue();
-    this.aaa.stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
-    console.log('msg sent',this.messageForm.getRawValue());
+    this.aaa.stompClient.send('/app/chat.sendMessage', {}, JSON.stringify(chatMessage));
+    console.log('msg sent', this.messageForm.getRawValue());
+    this.openDialog();
   }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ModalComponent, {
+      width: '250px',
+      data: {username: this.username}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.username = result;
+    });
+  }
+}
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './Modal.component.html'
+})
+
+export class ModalComponent {
+
+  constructor(public dialogRef: MatDialogRef<ModalComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
