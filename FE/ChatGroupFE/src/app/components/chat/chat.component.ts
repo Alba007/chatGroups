b45d599071621018eb5c5cdb6862264ad3058d4d
SamantaBeqiri <samanta29beqiri@gmail.com>
2019-07-29 10:30:37 +0200

    changes

--- FE/ChatGroupFE/src/app/components/chat/chat.component.ts
+++ FE/ChatGroupFE/src/app/components/chat/chat.component.ts
@@ -1,12 +1,13 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup} from '@angular/forms';
 import {WebSocketService} from '../../services/WebSocketService';
 import {getDataService} from '../../services/getDataService';
 import {Message, Type} from '../../models/message';
 import {HttpReqService} from 'src/app/services/http-req-service.service';
-import {GroupChatWSService} from "../../../groupChatWs";
-import {MatDialog, MatDialogConfig} from "@angular/material";
-import {AddGroupComponent} from "../add-group/add-group.component";
+import {GroupChatWSService} from '../../../groupChatWs';
+import {MatDialog, MatDialogConfig} from '@angular/material';
+import {AddGroupComponent} from '../add-group/add-group.component';
+import {GroupChat} from '../../models/GroupChat';
 
 
 @Component({
@@ -17,26 +18,30 @@ import {AddGroupComponent} from "../add-group/add-group.component";
 export class ChatComponent implements OnInit, AfterViewInit {
   public chatGroups = [];
   public chatMessages = [];
-  public chatId: string = "";
-  messageTobeShownInchat:any[]=[]
+  public chatId: string = '';
+  public oldChatId = '';
+  public join = false;
+  public showChat = false;
+  public activeGroup: GroupChat[] = [];
+  messageTobeShownInchat: any[] = [];
   usernameForm: FormGroup;
-  public username: string = "";
+  public username: string = '';
   //newMwssage
-  res:any
+  res: any;
   //position of message modified
-  pos:any
+  pos: any;
   messageForm: FormGroup;
   messageToBeSent: Message = {
-    "context": "",
-    "groupChatId": "",
-    "sender": "",
-    "time": "",
-    "type": Type.CHAT
+    'context': '',
+    'groupChatId': '',
+    'sender': '',
+    'time': '',
+    'type': Type.CHAT
   };
 
   constructor(private socketMessage: WebSocketService,
               private getDataService: getDataService,
-              private groupChatWs:GroupChatWSService,
+              private groupChatWs: GroupChatWSService,
               private dialog: MatDialog,
               private httpService: HttpReqService) {
 
@@ -44,12 +49,14 @@ export class ChatComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.socketMessage.connect();
-    this.groupChatWs.getMsgSubjectObservable().subscribe(x=>{
-      if(x){
-        console.log("x",x)
+    this.groupChatWs.getMsgSubjectObservable().subscribe(x => {
+      if (x) {
+        console.log("x", x)
         this.chatGroups.push(x)
       }
     });
+
+   
     this.messageForm = new FormGroup({
       message: new FormControl(''),
     });
@@ -57,38 +64,36 @@ export class ChatComponent implements OnInit, AfterViewInit {
     this.usernameForm = new FormGroup({
       username: new FormControl(''),
     });
-    this.socketMessage.newMessage.subscribe(newMessage=>{
+    this.socketMessage.newMessage.subscribe(newMessage => {
       //erdhi nje mesazh i ri nga nje chatues tjeter prandaj shtohet ne array
-      console.log(newMessage,"mesazhi per socket")
-      let that=this
-      if (newMessage.typee=="post"){
-             var newMess={
-               id: newMessage.id,
-               sender: newMessage.sender ,
-               context:newMessage.context,
-               type:newMessage.type,
-               groupChatId: newMessage.groupChatId,
-               time:newMessage.time
-             }
-        this.messageTobeShownInchat.push(newMess)
-      }
-      else {
-         //u modifikua nje mesazh 
-        var index = this.messageTobeShownInchat.map(function (current, index) {
+      console.log(newMessage, 'mesazhi per socket');
+      let that = this;
+      if (newMessage.typee == 'post') {
+        var newMess = {
+          id: newMessage.id,
+          sender: newMessage.sender,
+          context: newMessage.context,
+          type: newMessage.type,
+          groupChatId: newMessage.groupChatId,
+          time: newMessage.time
+        };
+        this.messageTobeShownInchat.push(newMess);
+      } else {
+        //u modifikua nje mesazh
+        var index = this.messageTobeShownInchat.map(function(current, index) {
           if (current.id == newMessage.id) {
             that.pos = index;
-            that.res = newMessage.context
-            return index
+            that.res = newMessage.context;
+            return index;
           }
-        })
-         this.notifyForEditMessage()
+        });
+        this.notifyForEditMessage();
       }
-      
-    })
-    if (localStorage.getItem('username')){
-            this.username = localStorage.getItem('username')
-    }
-    else{
+
+    });
+    if (localStorage.getItem('username')) {
+      this.username = localStorage.getItem('username');
+    } else {
 
     }
   }
@@ -100,7 +105,7 @@ export class ChatComponent implements OnInit, AfterViewInit {
 
   saveUsername() {
     this.username = this.usernameForm.getRawValue().username;
-    localStorage.setItem('username',this.username);  
+    // localStorage.setItem('username',this.username);
 
   }
 
@@ -109,75 +114,140 @@ export class ChatComponent implements OnInit, AfterViewInit {
     this.messageToBeSent.groupChatId = this.chatId;
     var today = new Date();
     var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
     var dateTime = date + 'T' + time;
     this.messageToBeSent.sender = this.username;
     this.messageToBeSent.time = dateTime;
     this.messageToBeSent.type = Type.CHAT;
-    this.getDataService.postMessages(this.messageToBeSent).subscribe()
+    this.getDataService.postMessages(this.messageToBeSent).subscribe(res=>{
+      this.messageForm.reset()
+    }
+      
+
+    );
+  }
+
+  isGroupActive(id): boolean {
+    const active = this.activeGroup.filter(x => x.id === id);
+    if (!active.length) {
+      return true;
+    }
+  }
+
+  removeGroup(group) {
+    this.showChat = false;
+    this.activeGroup = this.activeGroup.filter(x => x.id !== group.id);
+    this.messageToBeSent.context = this.username + ' left';
+    this.messageToBeSent.groupChatId = group.id;
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    // var dateTime = date + 'T' + time;
+    this.messageToBeSent.sender = '';
+    this.messageToBeSent.time = '';
+    this.messageToBeSent.type = Type.LEAVE;
+    this.getDataService.postMessages(this.messageToBeSent).subscribe();
+
   }
 
   openChat(group) {
-    this.chatId = group.id;
-    this.chatMessages=[];
-    this.messageTobeShownInchat=[]
-    this.httpService.getMessagesByChatId(this.chatId).subscribe(messages=>{
-     if (messages.length>0) {
-       this.chatMessages=messages 
-       //therritet metoda qe shton mesaZhet ne chat
-       this.addMessagesIntoChat() ;
+    this.chatId=group.id
+    this.showChat = true;
+    if (!this.join) {
+      this.activeGroup.push(group);
+      console.log(this.chatId, 'new');
+      console.log(this.oldChatId, 'old');
+      this.messageToBeSent.context = this.username + '  joined';
+      this.messageToBeSent.groupChatId = group.id;
+      var today = new Date();
+      var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+      this.messageToBeSent.sender = '';
+      this.messageToBeSent.time = '';
+      this.messageToBeSent.type = Type.JOIN;
+      this.getDataService.postMessages(this.messageToBeSent).subscribe();
+    }
+
+    if (this.isGroupActive(group.id)) {
+      this.activeGroup.push(group);
+      console.log(this.chatId, 'new');
+      console.log(this.oldChatId, 'old');
+      this.messageToBeSent.context = this.username + '  joined';
+      this.messageToBeSent.groupChatId = group.id;
+      var today = new Date();
+      var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+      this.messageToBeSent.sender = '';
+      this.messageToBeSent.time = '';
+      this.messageToBeSent.type = Type.JOIN;
+      this.getDataService.postMessages(this.messageToBeSent).subscribe();
+    }
+    this.join = true;
+    this.chatMessages = [];
+    this.messageTobeShownInchat = [];
+    this.httpService.getMessagesByChatId(group.id).subscribe(messages => {
+      if (messages.length > 0) {
+        this.chatMessages = messages;
+        console.log("mesagerr")
+        console.log(messages)
+        this.addMessagesIntoChat();
       }
-    })
+    });
   }
+
+  
 
   getData() {
     this.httpService.getGroups().subscribe(groups => {
       this.chatGroups = groups;
-    })
+    });
   }
 
   addMessagesIntoChat() {
-    this.messageTobeShownInchat=this.chatMessages.map(function(current){
-              return current
-               
-    })
- console.log(this.messageTobeShownInchat)
+    this.messageTobeShownInchat = this.chatMessages.map(function(current) {
+      return current;
+
+    });
+    console.log(this.messageTobeShownInchat);
   }
 
-  addGroup(){
+  addGroup() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
-    dialogConfig.width = "30%";
+    dialogConfig.width = '30%';
+    dialogConfig.data=this.chatGroups;
     this.dialog.open(AddGroupComponent, dialogConfig).afterClosed().subscribe(res => {
-    })
+    });
   }
-  editMesage(message){
-    let that = this
-    this.getDataService.openConfirmDialog().afterClosed().subscribe(res => {
-    
-      if (res!=""){
-          var index=this.messageTobeShownInchat.map(function(current, index){
-          if (current.id==message.id){
-            that.pos=index ;
-            that.res=res
-            return index
-          }
-        })
-        //
-        this.messageTobeShownInchat[this.pos].context = this.res
-        this.chatMessages[this.pos].context = this.res
-          this.getDataService.updateMessages(this.chatMessages[this.pos], this.chatMessages[this.pos].id).subscribe() ;
-      } 
-    
-     
 
+  editMesage(message) {
+    if(message.type !== 'CHAT'){
+      return;
     }
-    )
-      
+    let that = this;
+    this.getDataService.openConfirmDialog().afterClosed().subscribe(res => {
+
+        if (res != '') {
+          var index = this.messageTobeShownInchat.map(function(current, index) {
+            if (current.id == message.id) {
+              that.pos = index;
+              that.res = res;
+              return index;
+            }
+          });
+          //
+          this.messageTobeShownInchat[this.pos].context = this.res;
+          this.chatMessages[this.pos].context = this.res;
+          this.getDataService.updateMessages(this.chatMessages[this.pos], this.chatMessages[this.pos].id).subscribe();
+        }
+
+
+      }
+    );
+
   }
+
   notifyForEditMessage() {
-    
-    this.messageTobeShownInchat[this.pos].context = this.res
-    
+
+    this.messageTobeShownInchat[this.pos].context = this.res;
+
   }
 }
